41883652d45cf70fb13f6ef625cc23089e6816f0
RichoDemus <richodemus@gmail.com>
2016-07-27 17:44:51 +0200

    Added some ugly api code

--- pokemon-go-adapter/src/main/kotlin/com/github/tsar_industries/poke_radar/pokemon_go/PokemonGoService.kt
+++ pokemon-go-adapter/src/main/kotlin/com/github/tsar_industries/poke_radar/pokemon_go/PokemonGoService.kt
@@ -6,13 +6,26 @@ import okhttp3.OkHttpClient
 
 
 open class PokemonGoService {
+    var pokemonApi: PokemonGo? = null
+
+
     fun getUsername(username: String, password: String): String {
 
         val myPair = Pair(1, 2) //todo remove this once we use kotlin stdlib properly somewhere
 
         val client = OkHttpClient()
         val provider = PtcCredentialProvider(client, username, password)
-        val pokemonApi = PokemonGo(provider, client)
-        return pokemonApi.playerProfile.username
+        pokemonApi = PokemonGo(provider, client)
+
+        return pokemonApi!!.playerProfile.username
+    }
+
+    fun setLocation() {
+        pokemonApi!!.setLocation(59.337042, 18.064270, 0.0)
+    }
+
+    fun getCatchablePokemons(): List<String> {
+        return pokemonApi!!.map.catchablePokemon
+                .map { it.pokemonId.name }
     }
 }
